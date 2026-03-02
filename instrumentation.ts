d9be9a7805029097c8faf7ce8be0202028305e58
Danylo S <danylo.sovgut@bmg.com>
2026-03-02 11:53:20 -0500

    dealeate

--- instrumentation.ts
+++
@@ -1,6 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    const { registerScheduler } = await import("./lib/scheduler");
-    registerScheduler();
-  }
-}
